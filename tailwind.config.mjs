b7384098012acc3c6b196be1b290a7d710d7706a
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-01-29 00:40:22 +0530

    update shades and font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,26 @@
+// tailwind.config.mjs
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'media',
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        "primary-dark": "var(--primary-dark)",
+        accent: "var(--accent)",
+        white: "var(--white)",
+        "dark-gray": "var(--dark-gray)",
+      },
+      fontFamily: {
+        serif: ["var(--font-source-serif-pro)", "serif"],
       },
     },
   },
