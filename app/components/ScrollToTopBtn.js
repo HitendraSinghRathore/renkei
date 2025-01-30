2bb2e8b02e443504dafca98c47f00331983b06c7
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-01-30 10:13:16 +0530

    update changes

---
+++ app/components/ScrollToTopBtn.js
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+
+export default function ScrollToTopButton() {
+  function handleScrollTop(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleScrollTop}
+      aria-label="Scroll to Top"
+      className="
+        font-serif
+        sr-only
+        focus:not-sr-only
+        focus:absolute
+        focus:top-6
+        focus:left-4
+        focus:z-50
+        bg-primary
+        text-white
+        text-base
+        sm:text-lg
+        md:text-xl
+        lg:text-2xl
+        rounded
+      "
+      tabIndex="0"
+    >
+      Scroll to Top
+    </button>
+  );
+}
