4df740841a73ecd789085c6bec62dbab60fe023f
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-01-31 07:35:26 +0530

    updated

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -14,7 +14,7 @@ export default function loginComponent() {
     <div className="h-screen overflow-hidden">
       <MainNav/>
       <section className="bg-gray-100 w-full h-full py-20 px-2 sm:px-4 md:px-8">
-        <div className="mx-auto bg-white rounded-lg shadow pt-8 p-6  md:p-10 border-2 border-primary w-full sm:w-2/3 max-w-md">
+        <div className="mx-auto bg-white rounded-lg shadow pt-8 p-6  md:p-10 w-full sm:w-2/3 max-w-md">
             <h1 className="text-2xl sm:text-3xl text-gray-800 mb-2 text-primary-dark">Sign in </h1>
            <p className="text-md text-gray-600 mb-8 text-muted">Enter your details to start using Renkei.</p>
            <Label htmlFor="email" className="mb-4 text-md text-gray-600">Email</Label>
