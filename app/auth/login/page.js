e2728b0370fe8af4d892be120fe0bb7e0d7c6fc6
HitendraSinghRathore <hitendersingh2@gmail.com>
2025-01-31 07:33:12 +0530

    updated style

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,4 +1,4 @@
-import { Eye, EyeOff } from "lucide-react";
+import { Eye } from "lucide-react";
 import Link from "next/link";
 
 import MainNav from "@/app/components/layouts/MainNav";
@@ -14,7 +14,7 @@ export default function loginComponent() {
     <div className="h-screen overflow-hidden">
       <MainNav/>
       <section className="bg-gray-100 w-full h-full py-20 px-2 sm:px-4 md:px-8">
-        <div className="mx-auto bg-white rounded-lg shadow pt-8 p-6  md:p-10 border border-gray-200 w-full sm:w-2/3 max-w-md">
+        <div className="mx-auto bg-white rounded-lg shadow pt-8 p-6  md:p-10 border-2 border-primary w-full sm:w-2/3 max-w-md">
             <h1 className="text-2xl sm:text-3xl text-gray-800 mb-2 text-primary-dark">Sign in </h1>
            <p className="text-md text-gray-600 mb-8 text-muted">Enter your details to start using Renkei.</p>
            <Label htmlFor="email" className="mb-4 text-md text-gray-600">Email</Label>
@@ -31,7 +31,8 @@ export default function loginComponent() {
           </button>
            </div>
           <button className="bg-primary text-white rounded-md py-2 px-4 w-full hover:bg-pink-600 hover:shadow-md transition mb-4">Sign in</button>
-          <button className="bg-white border border-primary  rounded-sm md:rounded-md py-2 px-4   w-full text-primary text-sm hover:bg-pink-100 hover:shadow-md transition mb-4">Sign in with Google</button>
+          <button className="bg-white border border-primary  rounded-sm md:rounded-md py-2 px-4   w-full text-primary  hover:bg-pink-100 hover:shadow-md transition mb-4">
+            Sign in with Google</button>
           <Link className="text-primary text-sm sm:text-md" href="/auth/signup">
             Don&apos; t have an account? Sign up</Link>
         </div>
